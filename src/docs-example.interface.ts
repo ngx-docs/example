34b9a4e484bd6d63e19187f7de8f7cad4c0b5fe4
Diabeu <scibor.rudnicki@ster-project.pl>
2017-11-07 13:57:52 +0100

    refactor(DocsExampleInterface): add ViewInterface

--- src/docs-example.interface.ts
+++ src/docs-example.interface.ts
@@ -2,3 +2,15 @@ export interface LaunchInterface {
   location: string;
   tooltip?: string;
 };
+
+
+export interface ViewInterface {
+  code: {
+    active: boolean,
+    tooltip: string
+  };
+  debug: {
+    active: boolean,
+    tooltip: string
+  };
+}
