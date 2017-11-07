639384cd910aa7cd4e15366c70d85109268b5feb
Diabeu <scibor.rudnicki@ster-project.pl>
2017-11-07 13:57:25 +0100

    refactor(DocsExampleModule): add PACKAGE_CONFIG_TOKEN COMMON_DECLARATIONS_EXPORTS,
    feat: forRoot() method to provide config to style element

--- src/docs-example.module.ts
+++ src/docs-example.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Inject, InjectionToken, ModuleWithProviders, NgModule, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatIconModule, MatTabsModule, MatTooltipModule } from '@angular/material';
 
@@ -6,17 +6,21 @@ import { MatButtonModule, MatIconModule, MatTabsModule, MatTooltipModule } from 
 import { PrismModule } from '@ngx-prism/core';
 
 // internal
+import { PackageConfigInterface } from './package-config.interface';
 import { DocsExampleComponent } from './docs-example.component';
+
+export let PACKAGE_CONFIG_TOKEN = new InjectionToken<PackageConfigInterface>('package.config');
+
+const COMMON_DECLARATIONS_EXPORTS = [DocsExampleComponent];
 
 /**
  * @export
  * @class DocsExampleModule
  */
+//#region Module
 @NgModule({
-  declarations: [ DocsExampleComponent ],
-  exports: [
-    DocsExampleComponent
-  ],
+  declarations: COMMON_DECLARATIONS_EXPORTS,
+  exports: COMMON_DECLARATIONS_EXPORTS,
   imports: [
     CommonModule,
 
@@ -28,7 +32,23 @@ import { DocsExampleComponent } from './docs-example.component';
 
     // @ngx
     PrismModule
-  ],
-  providers: [],
+  ]
 })
-export class DocsExampleModule { }
+export class DocsExampleModule {
+
+  /**
+   * @static
+   * @param {PackageConfig} [config]
+   * @returns {ModuleWithProviders}
+   * @memberof DocsExampleModule
+   */
+  static forRoot( @Optional() config?: PackageConfigInterface): ModuleWithProviders {
+    return {
+      ngModule: DocsExampleModule,
+      providers: [
+        { provide: PACKAGE_CONFIG_TOKEN, useValue: config }
+      ]
+    };
+  }
+}
+//#endregion
