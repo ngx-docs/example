060ce49255c6ce847ad9e4149c3811b37d462b31
Diabeu <scibor.rudnicki@ster-project.pl>
2017-12-18 17:56:26 +0100

    refactor(AppModule): add FlexLayoutModule, DocsApiModule with more config options

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // added
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatInputModule } from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSidenavModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+} from '@angular/material'; // added
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // added
+import { FlexLayoutModule } from '@angular/flex-layout'; // added
 
 // @ngx
 import { DocsExampleModule } from '@ngx-docs/example'; // added
+import { DocsApiModule } from '@ngx-docs/api';
 
 // internal
 import { AppComponent } from './app.component';
@@ -19,15 +27,24 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserAnimationsModule, // added
     BrowserModule,
+    FlexLayoutModule,
     FormsModule, // added
     ReactiveFormsModule,
+
+    DocsApiModule,
     DocsExampleModule.forRoot({
-      border: '1px solid red'
+      border: '1px solid red',
+      body_font_size: '0.875em',
+      box_shadow: '0 0 30px #aaa',
+      source_font_size: '0.875em'
     }), // added
 
     MatButtonModule, // added
+    MatIconModule, // added
     MatInputModule, // added
-    MatSidenavModule // added
+    MatSelectModule, // added
+    MatSidenavModule, // added
+    MatSliderModule // added
   ],
   providers: [],
   bootstrap: [AppComponent]
